65accc07805e7db79ef7d907d12726fdcacb30c2
baaelbre <44871173+baaelbre@users.noreply.github.com>
2021-01-22 12:18:32 +0100

    Update symplectic_decomposition.m

--- Entanglement_truncation/Matlab/symplectic_decomposition.m
+++ Entanglement_truncation/Matlab/symplectic_decomposition.m
@@ -1,13 +1,17 @@
-clear
-clc
-
 mu = 1;
 nu = 0.4;
-D = 1;
+D = 2;
 
-data = dlmread('../../Variational_optimization/Matlab/nonrelboson_real/results/data');
-mask = boolean((data(:,1) == mu).*(data(:,2) == nu).*(data(:,3) == 1));
-e_gctns = data(mask,4);
-data = dlmread(strcat('../../Variational_optimization/Matlab/nonrelboson_real/results/',num2str(mu),'_',num2str(nu),'_1_1'),',');
-V = data(1:D);
+% dlmread reads data into a matrix, strcat concatenates a string
+data = dlmread(strcat('../../Variational_optimization/Matlab/nonrelboson_real/results/',num2str(mu),'_',num2str(nu),'_', num2str(D), '_1'));
+% construct coupling matrix 
+V = diag(data(1:D));
 alpha = data(D+1:2*D);
+M = [[V,-kron(alpha,alpha')];[-kron(alpha',alpha),V]];
+Omega = M^(1/2); %2Dx2D matrices
+Omega_inv = M^(-1/2);
+% RDM: only keep upper left blocks of Omega(_inv) in covariance matrix
+X = Omega_inv(1:D, 1:D);
+P = Omega(1:D, 1:D);
+% and now X != P^-1 
+disp(X), disp(Omega_inv), disp(P);
