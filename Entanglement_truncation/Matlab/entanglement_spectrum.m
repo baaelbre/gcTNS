05ab0891576aed4d19306859bfc93cdcc131ed29
baaelbre <44871173+baaelbre@users.noreply.github.com>
2021-01-31 00:02:59 +0100

    plot schmidt spectrum
    very brute, nmax determines how many particles in each mode. schmidt_max how many schmidt modes you plot.
    
    On the x-axis the corresponding state of a particular singular value is shown

--- Entanglement_truncation/Matlab/entanglement_spectrum.m
+++ Entanglement_truncation/Matlab/entanglement_spectrum.m
@@ -3,7 +3,7 @@ clc
 
 mu = 1;
 nu = 0.4;
-D = 2;
+D = 3;
 % number of iterations: (nmax+1)^D (so can go pretty big as D is small in
 % general)
 nmax = 4 ;
@@ -43,6 +43,34 @@ end
 states = states(sortIdx);
 celldisp(states);
 
-schmidt_nmbr = linspace(1,5,5);
+schmidt_max = 5;
+schmidt_nmbr = linspace(1,schmidt_max,schmidt_max);
+
+ticks = {};
+% construct list of state strings
+for i = 1:schmidt_max
+    ket = states{i};
+    str = "";
+    str = str + '$$|'
+    for j = 1:D
+        str = str + num2str(ket(j)) + ', ';
+    end
+    temp = char(str);
+    temp = temp(1:end-2);
+    str = string(temp);
+    str = str+'>$$';
+    ticks{i} = str;
+end
+celldisp(ticks)
+
+set(groot,'defaultAxesTickLabelInterpreter','latex');  
+figure();
+plot(schmidt_nmbr, schmidts(1:schmidt_max),'.', 'MarkerSize', 20);
+grid on;
+xlabel("state i",'Interpreter','Latex','FontSize',20);
+xticks(schmidt_nmbr);
+xticklabels(ticks);
+ylabel("Schmidt value $$\lambda_i$$",'Interpreter','Latex','FontSize',20);
+title(strcat('$D=$', num2str(D), ' simulation with', ' $\mu=$ ', num2str(mu), ', $\nu=$ ', num2str(nu)), 'Interpreter','Latex','FontSize',20);
 
 % now calculate for each state the corresponding schmidt value
